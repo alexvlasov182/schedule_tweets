59e230bbfe77041ca0abd38a06e26bc01d1de9c7
Alex Vlasov <drumlife182@gmail.com>
2023-02-05 15:55:43 +0100

    Added flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
-  def index; end
+  def index
+    flash[:notice] = 'Logged in successfully'
+    flash[:alert] = 'Invalid email or password'
+  end
 end
